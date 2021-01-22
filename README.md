ff184d7ba2b6d7aa34efa5b991918bb508013f39
Shravan Asati <delunator.one@yahoo.com>
2021-01-22 20:16:43 +0530

    version 1

---
+++ README.md
@@ -0,0 +1,42 @@
+# Ecommerce-Price-Tracker
+
+**Ecommerce-Price-Tracker** is a python utility which notifies you when the price of the product on your wishlist is less than what you desired. It has support for price tracking on Amazon and Flipkart.
+
+## Features
+- Track prices of a product on Amazon
+- Track prices of a product on Flipkart
+- Track prices of a product on both Amazon and Flipkart simultaneously
+- Get notified via emails when the price is lower than desired price
+
+## Setup
+To use the **Ecommerce-Price-Tracker**, you just need the product name, product url and the desired price.
+
+* Product name - The name of the product you want to track
+* Product URL - The URL of the product page on Amazon or Flipkart
+* Desired price - The least price you want to buy the product at. When this price will be lower or eqaul to the actual price of the product, you'll be notified via a email.
+
+**Also provide your email and email password in the `PriceTracker` class, and enable less secure apps on your email service provider before running the program.**
+
+## Dependencies
+*External modules used:*
+- beautifulsoup4
+- helium
+- requests 
+- validators 
+- html5lib 
+
+Download all the above mentioned modules at once by executing the command `pip install -r requirements.txt` on the terminal.
+
+
+## Installation
+### Using Git
+Type the following command in your Git Bash:
+
+- For SSH:
+```git clone git@github.com:Shravan-1908/Ecommerce-Price-Tracker.git```
+- For HTTPS: ```git clone https://github.com/Shravan-1908/Ecommerce-Price-Tracker.git```
+
+The whole repository would be cloned in the directory you opened the Git Bash in.
+
+### Using GitHub ZIP download
+You can alternatively download the repository as a zip file using the GitHub **Download ZIP** feature by clicking [here](https://github.com/Shravan-1908/Ecommerce-Price-Tracker/archive/master.zip).
---
+++ main.py
@@ -0,0 +1,208 @@
+from abc import ABC, abstractmethod
+from helium import start_chrome, kill_browser
+from bs4 import BeautifulSoup
+from datetime import datetime
+import smtplib
+import validators
+from time import sleep
+from threading import Thread
+import re
+import requests
+import os
+
+
+class PriceTracker(ABC):
+    """
+    Base class for Price Trackers.
+    """
+    email = "your email here"
+    password = "your email password here"
+    removals = re.compile(r"₹|,|[$]")
+
+    def __init__(self, product_name: str, product_url: str, desired_price: int) -> None:
+        """
+        Constructor.
+        """
+        if not validators.url(product_url):
+            raise Exception("Invalid URL!")
+
+        self.product_url = product_url
+        self.set_price = desired_price
+        self.product_name = product_name
+        self.price = None
+
+    @abstractmethod
+    def get_price(self):
+        """
+        Fetches the latest price of the product.
+        """
+        pass
+
+    @staticmethod
+    def send_mail(your_email:str, your_password:str, subject: str, body: str):
+        """
+        Sends mail for the given price.
+        """
+        server = smtplib.SMTP('smtp.mail.yahoo.com', 587)
+        server.ehlo()
+        server.starttls()
+        server.login(your_email, your_password)
+        server.sendmail(your_email,
+                        your_email, f"Subject: {subject}\n\n{body}")
+        server.quit()
+        print("Mail sent")
+
+    @abstractmethod
+    def write_to_file(self):
+        """
+        Write content to a text file.
+        """
+        pass
+
+    def track_price(self):
+        """
+        Tracks price for a given product against the set price.
+        """
+        while True:
+            self.get_price()
+            if self.price == None:
+                print("Unable to fetch the latest price!")
+                os._exit(1)
+
+            if self.set_price >= self.price:
+                print("Price low for", self.product_name)
+                self.write_to_file()
+                self.send_mail(self.email, self.password, f"Price down for {self.product_name}",f"Dear sir,\nThe price for `{self.product_name}` is now {self.price} which is less than or equal to what you desired, {self.set_price}! Visit {self.product_url} for more info.")
+                os._exit(0)
+
+            else:
+                self.write_to_file()
+
+            sleep(60)
+
+
+class AmazonPriceTracker(PriceTracker):
+    """
+    Helps poor people by notifying them when the price for their favorite product on Amazon is less than what they desired.
+    """
+
+    def get_price(self):
+        """
+        Fetches price for given product on Amazon.
+        """
+        driver = start_chrome(self.product_url)
+        html = driver.page_source.replace("&nbsp;", "")
+        kill_browser()
+        soup = BeautifulSoup(html, "html5lib")
+        price = soup.find("span", {"id": "priceblock_ourprice"})
+        if price == None:
+            price = soup.find("span", {"id": "priceblock_dealprice"}).string
+        price = (re.sub(self.removals, "", price)).replace("\\xa", "")
+        self.price = int(float(price))
+
+    def write_to_file(self):
+        """
+        Writes datetime and price to a file.
+        """
+        now = datetime.now().strftime("%d-%m-%y %H:%M")
+        content = f"{now} --> Amazon --> {self.price}\n"
+        print(content)
+        with open(f"{self.product_name}.prices", "a") as f:
+            f.write(content)
+
+    def run(self):
+        self.track_price()
+
+
+class FlipkartPriceTracker(PriceTracker):
+    """
+    Helps poor people by notifying them when the price of their favorite product on Flipkart is less than what they desired.
+    """
+
+    def get_price(self):
+        """
+        Fetches price for given product on Flipkart.
+        """
+        r = requests.get(self.product_url)
+        soup = BeautifulSoup(r.content, "html5lib")
+        price = soup.find("div", {"class": "_30jeq3 _16Jk6d"}).string
+        price = re.sub(self.removals, "", price)
+        self.price = int(price)
+
+    def write_to_file(self):
+        """
+        Writes datetime and price to a file.
+        """
+        now = datetime.now().strftime("%d-%m-%y %H:%M")
+        content = f"{now} --> Flipkart --> {self.price}\n"
+        print(content)
+        with open(f"{self.product_name}.prices", "a") as f:
+            f.write(content)
+
+    def run(self):
+        self.track_price()
+
+
+class MultipleStorePriceTracker():
+    """
+    Tracks prices for two stores at a time (Amazon and Flipkart) and notifies when either of them is less than the desired price.
+    """
+
+    def __init__(self, product_name:str, amazon_url:str, flipkart_url:str, desired_price:int) -> None:
+        self.product = product_name
+        self.fkturl = flipkart_url
+        self.azurl = amazon_url
+        self.set_price = desired_price
+
+    def track_multiple(self):
+        fktt = FlipkartPriceTracker(self.product, self.fkturl, self.set_price)
+        azt = AmazonPriceTracker(self.product, self.azurl, self.set_price)
+        t1 = Thread(target=fktt.run)
+        t2 = Thread(target=azt.run)
+        t1.start()
+        t2.start()
+
+
+
+if __name__ == "__main__":
+
+    print("Welcome to the Ecommerce Price Tracker! A tool which you can use to track prices for a given product on Amazon and Flipkat.\n")
+
+    track_opt = input("Where to track prices?\n 1. Amazon \n 2. Flipkart \n 3. Both\n")
+
+    if track_opt == "1":
+        try:
+            product_name = input("Enter product name: ")
+            product_url = input("Enter the product url: ")
+            desired_price = int(input("Enter the desired price (you'll be notified when this price is lower than the product price): "))
+            azt = AmazonPriceTracker(product_name, product_url, desired_price)
+            azt.run()
+
+        except Exception as e:
+            print(e)
+
+    elif track_opt == "2":
+        try:
+            product_name = input("Enter product name: ")
+            product_url = input("Enter the product url: ")
+            desired_price = int(input("Enter the desired price (you'll be notified when this price is lower than the product price): "))
+            fkt = FlipkartPriceTracker(product_name, product_url, desired_price)
+            fkt.run()
+        
+        except Exception as e:
+            print(e)
+
+    elif track_opt == "3":
+        try:
+            product_name = input("Enter product name: ")
+            azurl = input("Enter the product url on Amazon: ")
+            fkturl = input("Enter the product url on Flipkart: ")
+            desired_price = int(input("Enter the desired price (you'll be notified when this price is lower than the product price): "))
+            mspt = MultipleStorePriceTracker(product_name, azurl, fkturl, desired_price)
+            mspt.track_multiple()
+
+        except Exception as e:
+            print(e)
+
+    else:
+        print("Invalid input!")
